e3ae24cb7c1cefa10589f78922395d175d2d61b4
Itahisa Diaz <maneit49@gmail.com>
2012-11-26 09:24:01 +0000

    tirada maquina

--- lib/ppt.rb
+++ lib/ppt.rb
@@ -3,5 +3,9 @@ class Ppt
   def humano_tira
 	 return @humano_tira
   end
+
+  def maquina_tira
+  	return @maquina_tira
+  end
   
 end
--- test/ppt_spec.rb
+++ test/ppt_spec.rb
@@ -4,4 +4,9 @@ describe Ppt do
   it "Debe existir una tirada para el humano" do
 	 Ppt.new().respond_to?("humano_tira").should == true
   end
+
+  it "Debe existir una tirada para la maquina" do
+	 Ppt.new().respond_to?("humano_tira").should == true
+  end
+
 end
